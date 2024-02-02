5ab15236e9367754e2b0fc06df6486f1179c0b26
arturiamu <arturiamumu@gmail.com>
2024-02-02 18:01:36 +0800

    simple

--- main.go
+++ main.go
@@ -1,1 +1,65 @@
-package bloom_go
+package main
+
+import (
+	"fmt"
+	"hash/fnv"
+)
+
+type BloomFilter struct {
+	bitArray []bool
+	hashList []BloomFilterHash
+}
+
+type BloomFilterHash func(s string) uint32
+
+func NewBloomFilter(size uint32) *BloomFilter {
+	return &BloomFilter{
+		bitArray: make([]bool, size),
+	}
+}
+
+func (b *BloomFilter) RegisterHash(h ...BloomFilterHash) {
+	b.hashList = append(b.hashList, h...)
+}
+
+func (b *BloomFilter) Add(s string) {
+	bitLen := uint32(len(b.bitArray))
+	for _, h := range b.hashList {
+		index := h(s) % bitLen
+		b.bitArray[index] = true
+	}
+}
+
+func (b *BloomFilter) Contains(s string) bool {
+	bitLen := uint32(len(b.bitArray))
+	for _, h := range b.hashList {
+		index := h(s) % bitLen
+		if !b.bitArray[index] {
+			return false
+		}
+	}
+	return true
+}
+
+func hash1(s string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return h.Sum32()
+}
+
+func hash2(s string) uint32 {
+	h := fnv.New32()
+	h.Write([]byte(s))
+	return h.Sum32()
+}
+
+func main() {
+	var data = "www.baidu.com"
+
+	bloom := NewBloomFilter(1 << 16)
+	bloom.RegisterHash(hash1, hash2)
+
+	bloom.Add(data)
+
+	fmt.Println(bloom.Contains(data))
+}
